301736f5e6df22e484831c9c1ff2bb6c5c6f200c
quweth <v.zakharchenko_fit_13_22_b_d@knute.edu.ua>
2023-11-16 16:09:16 +0200

    :wq

---
+++ LAB_01.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class LAB_01
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
